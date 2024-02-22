6fe6a71cb90658fe566b417822fd228885203220
valihuciu <valentinhuciu@gmail.com>
2024-02-22 21:02:35 +0200

    240222

--- src/test/java/pages/RegisterPage/RegisterPage.java
+++ src/test/java/pages/RegisterPage/RegisterPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pages.BasePage;
-import pages.SignInPage.SignInPage;
 
 public class RegisterPage extends BasePage {
     public static final Logger LOG = LoggerFactory.getLogger(RegisterPage.class);
@@ -22,13 +21,40 @@ public class RegisterPage extends BasePage {
     }
 
     private By inputFirstName = By.xpath("//input[@ng-model='FirstName']");
-    private By inputLastName = By.xpath("//input[@ng-model='LasttName']");
+    private By inputLastName = By.xpath("//input[@ng-model='LastName']");
+    private By consent = By.xpath("//p[text()='Consent']");
+    private By inputAddress = By.xpath("//textarea[@ng-model='Adress']");
+    private By inputEmail = By.xpath("//input[@type='email']");
+    private By inputPhoneNumber = By.xpath("//input[@ng-model='Phone']");
 
-    public void insertFullName(String fName, String lName){
+    public void insertFullName(String fName, String lName) {
         LOG.info("Inserting FirstName and LastName");
         driver.findElement(inputFirstName).sendKeys(fName);
         driver.findElement(inputLastName).sendKeys(lName);
     }
 
+    public void pressConsent() {
+        LOG.info("Press consent");
+        driver.findElement(consent).click();
+    }
+
+    public void insertAddress(String address) {
+        LOG.info("Insert adsress");
+        driver.findElement(inputAddress).sendKeys(address);
+    }
+
+    public void insertEmail(String email) {
+        LOG.info("Insert Email");
+        driver.findElement(inputEmail).sendKeys(email);
+    }
+
+    public void insertPhoneNaumber(String phone) {
+        LOG.info("Insert Phone number");
+        driver.findElement(inputPhoneNumber).sendKeys(phone);
+
+    }
+
 
 }
+
+
--- src/test/java/tests/RegisterTest/RegisterTest.java
+++ src/test/java/tests/RegisterTest/RegisterTest.java
@@ -7,19 +7,37 @@ import tests.BaseTest;
 
 import static pages.BasePage.*;
 
+
 public class RegisterTest extends BaseTest {
     public static final Logger LOG = LoggerFactory.getLogger(RegisterTest.class);
     private String newUrl = getBaseUrl() + "Register.html";
     String firstName = "Ion";
     String lastName = "Popescu";
+    String address = "Cluj, Str. Ion Barbu Nr.456";
+    String email = "tralala@domain.com";
+    String phone = "0121477582";
+
 
     @Test
     public void fillRegister() {
         driver.get(newUrl);
-        sleep( 3000L);
+        sleep(3000L);
+
+        LOG.info("Press consent");
+        registerPage.pressConsent();
 
         LOG.info("Typing Full Name");
         registerPage.insertFullName(firstName, lastName);
+
+        LOG.info("Type in Address");
+        registerPage.insertAddress(address);
+
+        LOG.info("Type in email");
+        registerPage.insertEmail(email);
+
+        LOG.info("Type in Phone Number");
+        registerPage.insertPhoneNaumber(phone);
+
 
     }
 }
