5b5c559b6faa5ae599e366bf1462b3db7ab31b2a
valihuciu <valentinhuciu@gmail.com>
2024-02-29 21:00:58 +0200

    240229

--- src/test/java/pages/RegisterPage/RegisterPage.java
+++ src/test/java/pages/RegisterPage/RegisterPage.java
@@ -1,6 +1,8 @@
 package pages.RegisterPage;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.Select;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pages.BasePage;
@@ -26,6 +28,25 @@ public class RegisterPage extends BasePage {
     private By inputAddress = By.xpath("//textarea[@ng-model='Adress']");
     private By inputEmail = By.xpath("//input[@type='email']");
     private By inputPhoneNumber = By.xpath("//input[@ng-model='Phone']");
+    private By maleGender = By.xpath("//input[@value='Male']");
+    private By femaleGender = By.xpath("//input[@value='FeMale']");
+    private By cricketCheckmarks = By.id("checkbox1");
+    private By moviesCheckmarks = By.id("checkbox2");
+    private By hockeyCheckmarks = By.id("checkbox3");
+    private By languageField = By.id("msdd");
+    String selectLanguage = "//a[contains(text(),'%s')]";
+    private By selectSkills = By.id("Skills");
+    private By selectCountry = By.xpath("//span[@role='combobox']");
+    private By insertCountry = By.xpath("//input[@role= 'textbox']");
+    private By selectYear = By.id("yearbox");
+    private By selectMonth = By.xpath("//select[@ng-model='monthbox']");
+    private By selectDay = By.id("daybox");
+    private By insertPasword = By.id("firstpassword");
+    private By insertConfirmPasword = By.id("secondpassword");
+    private By pressSubmit = By.id("submitbtn");
+    private By pressRefresh = By.xpath("//button[@value='Refresh']");
+
+
 
     public void insertFullName(String fName, String lName) {
         LOG.info("Inserting FirstName and LastName");
@@ -48,13 +69,83 @@ public class RegisterPage extends BasePage {
         driver.findElement(inputEmail).sendKeys(email);
     }
 
-    public void insertPhoneNaumber(String phone) {
+    public void insertPhoneNumber(String phone) {
         LOG.info("Insert Phone number");
         driver.findElement(inputPhoneNumber).sendKeys(phone);
+    }
+
+    public void setMaleGender() {
+        LOG.info("Clicking the male Radio button");
+        driver.findElement(maleGender).click();
+    }
+
+    public void setFemaleGender() {
+        LOG.info("Clicking the female radio button");
+        driver.findElement(femaleGender).click();
 
     }
 
+    public void clickCricket() {
+        LOG.info("Clicking cricket checkmarks");
+        driver.findElement(cricketCheckmarks).click();
+    }
 
+    public void clickMovies() {
+        LOG.info("Clicking movies checkmarks");
+        driver.findElement(moviesCheckmarks).click();
+    }
+
+    public void clickHockey() {
+        LOG.info("Clicking hockey checkmarks");
+        driver.findElement(hockeyCheckmarks).click();
+    }
+
+    public void selectLanguage(String language) {
+        LOG.info("Selecting language");
+        driver.findElement(languageField).click();
+        driver.findElement(By.xpath(String.format(selectLanguage, language)));
+    }
+
+    public void selectSkills(String skill) {
+        LOG.info("Selecting skill");
+        Select newSkill = new Select(driver.findElement(selectSkills));
+        newSkill.selectByValue(skill);
+    }
+
+    public void selectCountry(String country) {
+        LOG.info("Selecting country");
+        driver.findElement(selectCountry).click();
+        driver.findElement(insertCountry).sendKeys(country);
+        driver.findElement(selectCountry).sendKeys(Keys.ENTER);
+    }
+
+    public void setDateOfBirth(String year, String month, String day) {
+        LOG.info("Selecting date of birth");
+        Select newYear = new Select(driver.findElement(selectYear));
+        newYear.selectByValue(year);
+        Select newMonth = new Select(driver.findElement(selectMonth));
+        newMonth.selectByValue(month);
+        Select newDay = new Select(driver.findElement(selectDay));
+        newDay.selectByValue(day);
+    }
+
+    public void insertPassword(String password){
+        LOG.info("inserting password");
+        driver.findElement(insertPasword).sendKeys(password);
+    }
+
+    public void insertConfirmPassword(String confirmPassword){
+        LOG.info("inserting password");
+        driver.findElement(insertConfirmPasword).sendKeys(confirmPassword);
+    }
+
+    public void pressSubmit(){
+        LOG.info("pressing submit button");
+        driver.findElement(pressSubmit).click();
+    }
+
+    public void pressRefresh(){
+        LOG.info("pressing refresh button");
+        driver.findElement(pressRefresh).click();
+    }
 }
-
-
--- src/test/java/tests/RegisterTest/RegisterTest.java
+++ src/test/java/tests/RegisterTest/RegisterTest.java
@@ -16,6 +16,15 @@ public class RegisterTest extends BaseTest {
     String address = "Cluj, Str. Ion Barbu Nr.456";
     String email = "tralala@domain.com";
     String phone = "0121477582";
+    String language = "English";
+    String skill = "Java";
+    String country = "Japan";
+    String year = "1991";
+    String month = "February";
+    String day = "29";
+    String password = "P@ssw0rd";
+    String confirmPassword = "P@ssw0rd";
+
 
 
     @Test
@@ -36,8 +45,42 @@ public class RegisterTest extends BaseTest {
         registerPage.insertEmail(email);
 
         LOG.info("Type in Phone Number");
-        registerPage.insertPhoneNaumber(phone);
+        registerPage.insertPhoneNumber(phone);
 
+        LOG.info("Select Gender");
+        registerPage.setMaleGender();
+        registerPage.setFemaleGender();
+
+        LOG.info("Select Hobbies");
+        registerPage.clickCricket();
+        registerPage.clickMovies();
+        registerPage.clickHockey();
+
+        LOG.info("Select language");
+        registerPage.selectLanguage(language);
+
+        LOG.info("Select skill");
+        registerPage.selectSkills(skill);
+
+        LOG.info("Select country");
+        registerPage.selectCountry(country);
+
+        LOG.info("Select date of birth");
+        registerPage.setDateOfBirth(year, month, day);
+
+        LOG.info("Insert password");
+        registerPage.insertPassword(password);
+
+        LOG.info("Insert confirm password");
+        registerPage.insertConfirmPassword(confirmPassword);
+
+        LOG.info("press submit button");
+        registerPage.pressSubmit();
+
+        LOG.info("press refresh button");
+        registerPage.pressRefresh();
+
+        sleep(5000L);
 
     }
 }
